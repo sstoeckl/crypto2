797508d6a7bb6d531d6fd2993c78c2ae090aa656
sstoeckl <36445234+sstoeckl@users.noreply.github.com>
2022-10-25 14:50:28 +0000

    Deploying to gh-pages from @ sstoeckl/crypto2@795fd302d15a2f18b0be5eafc9b58e18cfe90c94 🚀

---
+++ reference/figures/fig.R
@@ -0,0 +1,5 @@
+library(hexSticker)
+imgurl <- "man/figures/ccs.png"
+sticker(imgurl, package="crypto2", p_size=30, s_x=1, s_y=.75, s_width=.4,
+        h_color="black",h_size = 2,h_fill = ,
+        filename="man/figures/crypto2_hex.png")
